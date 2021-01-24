c613956a2950016a30e26dbabafd798ac8870d3f
nms2811 <nms2811@gmail.com>
2021-01-24 04:08:11 -0500

    Updates

---
+++ precache-manifest.fdc133beb67fdc55a20905b13e70550c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "415d653852dea0523efe64f363d92686",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49ba0cd0aab74e03fa79",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "8a84e9c8764b4a0b291e",
+    "url": "/static/js/2.ff6cf792.chunk.js"
+  },
+  {
+    "revision": "f545a9e2181c332bd83ec648c9817514",
+    "url": "/static/js/2.ff6cf792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49ba0cd0aab74e03fa79",
+    "url": "/static/js/main.73e3282d.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
